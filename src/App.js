662446ce9169ebf0b4dfd695e9ed03cac9cd3647
kathleenconnaghan <77138134+kathleenconnaghan@users.noreply.github.com>
2021-11-30 15:58:53 -0600

    adds chat engine and links with chatengine.io API

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 const App = () => {
-    return ()
+    return (
+        <ChatEngine
+            height= '100vh'      // so the chat app will be the full height of the page
+            projectID= '7c2dfd2e-bfed-4d22-8df9-73c2e1c81114'     // projectID you got making a new project in chatengine.io 
+            userName= 'KathleenC'                                 // username you created in chatengine.io 
+            userSecret= '12345'                                   // secret you created with your user in chatengine.io
+        />
+
+    )
 }
 
 export default App;
