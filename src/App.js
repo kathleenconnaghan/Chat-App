8ecc662e938908613e3572b07a880eb4a90f74b1
kathleenconnaghan <77138134+kathleenconnaghan@users.noreply.github.com>
2021-12-17 14:43:49 -0600

    adds chatfeed, chatform and read receipts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ const App = () => {
             projectID= '7c2dfd2e-bfed-4d22-8df9-73c2e1c81114'     // projectID you got making a new project in chatengine.io 
             userName= 'KathleenC'                                 // username you created in chatengine.io 
             userSecret= '12345'                                   // secret you created with your user in chatengine.io
+            renderChatFeed= {( chatAppProps ) => <ChatFeed { ...chatAppProps } />}
         />
 
     )
